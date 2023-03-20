59bc21f302de1b7e846627db3b19cdfc8343feec
coditzeey <www.georgeokafor99@gmail.com>
2023-03-20 06:56:29 -0700

    commit in C

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <string.h>
 
 /**
@@ -7,25 +9,21 @@
  * Return: Always 0 (Success)
  */
 
-int main(void)
-
+int	main(void)
 {
+	int i;
 
-    int i;
+	for (i = 97; i < 123; i++)
 
-    for (i = 97; i < 123; i++)
+	{
+		if (i != 101 && i != 113)
 
-    {
+		{
+			putchar(i);
+		}
+	}
 
-        if (i != 101 && i != 113)
+	putchar('\n');
 
-        {
-
-            putchar(i);
-        }
-    }
-
-    putchar('\n');
-
-    return (0);
+	return (0);
 }
